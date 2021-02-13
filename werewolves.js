2e3d5dbf011e6f85a11360e9afdecf98cfb43a1c
Davidholder1986 <davidholder24@gmail.com>
2021-02-13 13:18:19 +0000

    voting update

--- werewolves.js
+++ werewolves.js
@@ -10,12 +10,13 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 var people_logged_in = {};
 var num_people_connected = 0;
 var players_voted = 0;
+var players_alive = 0
+var votes = {}
 
 function AllPeopleReady() {
     for (id in people_logged_in) {
         id = people_logged_in[id]
         if (id.gameReady == false) {
-            console.log(id)
             return false;
         }
     }
@@ -30,8 +31,34 @@ function AllPeopleReady() {
         }
         count += 1
     }
-    console.log('All Ready')
     return true;
+}
+
+function kill_voted_out_player() {
+    const max_votes = Math.max.apply(null, Object.values(votes));
+    console.log('max votes: ' + max_votes)
+    Object.entries(votes).forEach(([key, value]) => {
+        if (value >= max_votes) {
+            for (id in people_logged_in) {
+                id = people_logged_in[id]
+                console.log('key: ' + key)
+                console.log('name: ' + id.name)
+                if (id.name == key) {
+                    id.status = 'dead';
+                    console.log('is dead: ' + id.name)
+                }
+            }
+        }
+    });
+}
+
+function update_players_alive() {
+    for (id in people_logged_in) {
+        id = people_logged_in[id]
+        if (id.status == 'alive') {
+            players_alive += 1
+        }
+    }
 }
 
 io.on('connection', (socket) => {
@@ -39,15 +66,13 @@ io.on('connection', (socket) => {
     num_people_connected += 1;
 
     socket.on('name', (name) => {
-        people_logged_in[socket.id] = { 'name' : name, gameReady: false, gameStarted: false, index: 0, person: 'villager', status: 'alive' };
+        people_logged_in[socket.id] = { 'name' : name, gameReady: false, gameStarted: false, index: 0, person: 'villager', status: 'alive'};
         console.log(people_logged_in);
     });
 
     socket.on('personReady', (name) => {
         people_logged_in[socket.id].gameReady = true;
-        console.log(socket.id)
         if (AllPeopleReady()) {
-            console.log(name + ' ready')
             io.emit('gameStarting', (people_logged_in))
         }
     });
@@ -55,25 +80,35 @@ io.on('connection', (socket) => {
     socket.on('playerKilled', (name) => {
         for (id in people_logged_in) {
             id = people_logged_in[id]
-            console.log(id.name);
             if (id.name == name) {
                 id.status = 'dead';
             }
+        }
+        update_players_alive()
+        if (players_alive == 1)
+        {
+            io.emit('gameOver', (people_logged_in))
         }
         io.emit('playerDeathUpdate', (people_logged_in))
 
     });
 
     socket.on('playerVoted', (name) => {
-        for (id in people_logged_in) {
-            id = people_logged_in[id]
-            console.log(id.name);
-            if (id.name == name) {
-                id.status = 'dead';
-            }
+        if (votes[name] >= 1) {
+            votes[name] += 1
+        } else {
+            votes[name] = 1;
         }
+
         players_voted += 1
+
+        console.log('players voted: ' + players_voted)
+        console.log(players_voted == num_people_connected)
+
         if (players_voted == num_people_connected) {
+            kill_voted_out_player()
+            console.log('test')
+            console.log(people_logged_in)
             io.emit('playerVoteUpdate', (people_logged_in))
         }
 
